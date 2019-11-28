2fdb13db5663d0465e527e1cfb99bf6fe9ebf189
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 20:02:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,7 +48,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   i = 0 
-  total = 0 
+  total = starting_point 
  
  while i < source_array.length do 
    total = total + source_array[i]  
