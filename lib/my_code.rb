33effe4c6ef8c5aa996a3865045b929cd4ff8572
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 19:52:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,13 @@ def map_to_negativize(source_array)
  i = 0 
  negative_array = [] 
  
+ while i < source_array.length do 
+   negative_array << (source_array[i]*-1)  
+   
+   
+   i += 1 
+ end
+ 
 end
 
 
