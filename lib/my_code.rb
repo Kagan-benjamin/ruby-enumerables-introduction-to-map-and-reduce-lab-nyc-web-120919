1aafbd23b67963f6f0e579242d757c23690470bf
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 19:47:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,19 @@ def map_to_square(source_array)
   
 end
 
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+
+def reduce_to_all_true(source_array)
+  
+end
+
+
+def reduce_to_any_true(source_array)
+  
+end
+
+
