642737d246ea3ab64faa5605f6f8b4366f1a431b
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 20:09:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -72,7 +72,15 @@ end
 
 
 def reduce_to_any_true(source_array)
-  
+   i = 0 
+ 
+ while i < source_array.length do 
+   if !source_array[i]  
+     return false
+   end 
+   i += 1 
+ end
+ return true 
 end
 
 
