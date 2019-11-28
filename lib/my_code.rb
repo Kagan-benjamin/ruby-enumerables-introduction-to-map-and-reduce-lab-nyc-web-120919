a0bd91abb6ddce0cae1241ea0bc136feb8255a2a
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 19:56:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,7 +35,13 @@ end
 
 
 def map_to_square(source_array)
-  
+  i = 0 
+ negative_array = [] 
+ 
+ while i < source_array.length do 
+   negative_array << (source_array[i]*-1)  
+   i += 1 
+ end
 end
 
 
