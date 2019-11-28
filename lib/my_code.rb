160b5e41b6758c961b58340b8922be3345b9e6d3
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 19:55:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,7 +23,13 @@ end
 
 
 def map_to_double(source_array)
-  
+  i = 0 
+ negative_array = [] 
+ 
+ while i < source_array.length do 
+   negative_array << (source_array[i]*-1)  
+   i += 1 
+ end
 end
 
 
