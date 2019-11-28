1f76cd5fe515ba5c7b8c50d7f55ac9728f0ec56e
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-28 19:53:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,13 @@ end
 
 
 def map_to_no_change(source_array)
-  
+  i = 0 
+ negative_array = [] 
+ 
+ while i < source_array.length do 
+   negative_array << (source_array[i]*-1)  
+   i += 1 
+ end
 end
 
 
